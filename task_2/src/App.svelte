5d9f4b9e731a0f694efc2ec793a3979a20e392f4
nnder <asfahaikrkt6k56@yandex.ru>
2024-04-26 09:10:18 +0500

    basic form

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,47 +1,73 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import {round} from 'mathjs'
+
+  $: currency1 = 90
+  $: currency2 = 1
+
+  $: result1 = 90
+  $: result2 = 1
+
+  function handleCurrent1(e){
+    currency1 = e.target.value
+
+    
+    result2 = result1 / currency1
+  }
+
+  function handleCurrent2(e){
+    currency2 = e.target.value
+
+    result1 = round(result1 / (currency2 * result2 ), 2)
+  }
+
+  function res1(e){
+    result1 = e.target.value
+    result2 = round(result1 / currency2, 2)
+  }
+
+  function res2(e){
+    result2 = e.target.value
+    result1 = round(result2 * currency1, 2)
+  }
+
+  
+
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
   <div class="card">
-    <Counter />
+    <div>
+      <select on:change={handleCurrent1}>
+        <option value="1">rub</option>
+        <option value="90">usd</option>
+        <option value="97">euro</option>
+      </select>
+
+      <input type="number" value={result1} on:change={res1}>
+    </div>
+
+    <div>
+      <select on:change={handleCurrent2}>
+        <option value="90">usd</option>
+        <option value="97">euro</option>
+        <option value="1">rub</option>
+      </select>
+
+      <input type="number" value={result2} on:change={res2}>
+    </div>
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+  <div>
+    {currency1}
+    {result1}
+  </div>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <div>
+    {currency2}
+    {result2}
+  </div>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
