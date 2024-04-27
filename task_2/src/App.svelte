257c77952eb3db492e4d61481e7e149704549b40
nnder <asfahaikrkt6k56@yandex.ru>
2024-04-27 06:56:16 +0500

    normal transform

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -4,32 +4,56 @@
   $: currency1 = 90
   $: currency2 = 1
 
-  $: result1 = 90
-  $: result2 = 1
+  $: result1 = 1
+  $: result2 = 90
 
   function handleCurrent1(e){
     currency1 = e.target.value
-
-    
-    result2 = result1 / currency1
+    if(currency1 == currency2){
+      result1 = 1
+      result2 = 1
+    } else {
+      result1 = 1
+      let exchange = round(currency1 > currency2 ? currency1: currency2 / currency1, 2)
+      result2 = round(result1 * exchange, 2)
+    }
   }
 
   function handleCurrent2(e){
     currency2 = e.target.value
-
-    result1 = round(result1 / (currency2 * result2 ), 2)
+    console.log(currency1 == currency2)
+    if(currency1 == currency2){
+      result1 = 1
+      result2 = 1
+    } else {
+      result2 = 1
+      let exchange = round(currency1 > currency2 ? currency1 / currency2 : currency2 / currency1, 2)
+      result1 = round(result2 * exchange , 2)
+    }
   }
 
   function res1(e){
     result1 = e.target.value
-    result2 = round(result1 / currency2, 2)
+    if(currency1 >= currency2){
+      result2 = round(result1 * currency1, 2)
+    } else {
+      result2 = round(result1 / (currency2 / currency1 ), 2)
+    }
   }
 
   function res2(e){
     result2 = e.target.value
-    result1 = round(result2 * currency1, 2)
+    if(currency1 >= currency2){
+      result1 = round(result2 / currency1, 2)
+    } else {
+      result1 = round(result2 * (currency2 / currency1 ), 2)
+    }
+    
   }
 
+
+  // 1=97
+  // 1.07=90
   
 
 </script>
@@ -38,8 +62,8 @@
   <div class="card">
     <div>
       <select on:change={handleCurrent1}>
-        <option value="1">rub</option>
         <option value="90">usd</option>
+        <option value="1">rub</option>
         <option value="97">euro</option>
       </select>
 
@@ -48,9 +72,9 @@
 
     <div>
       <select on:change={handleCurrent2}>
+        <option value="1">rub</option>
         <option value="90">usd</option>
         <option value="97">euro</option>
-        <option value="1">rub</option>
       </select>
 
       <input type="number" value={result2} on:change={res2}>
