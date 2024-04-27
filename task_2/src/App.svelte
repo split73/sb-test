47d47368676ee2dbc6b86db9ce3e4ec88633e058
nnder <asfahaikrkt6k56@yandex.ru>
2024-04-27 21:57:37 +0500

    get data

--- task_2/src/App.svelte
+++ task_2/src/App.svelte
@@ -1,60 +1,74 @@
 <script lang="ts">
   import {round} from 'mathjs'
+  import axios from 'axios';
 
-  $: currency1 = 90
+  $: selected1 = "RUB"
+  $: selected2 = "RUB"
+
+  $: currency1 = 1
   $: currency2 = 1
 
   $: result1 = 1
-  $: result2 = 90
+  $: result2 = 1
 
-  function handleCurrent1(e){
-    currency1 = e.target.value
-    if(currency1 == currency2){
+  $: flag = true
+
+  async function getCurrency(currency1: string, currency2: string){
+    let response = await axios.get(`https://open.er-api.com/v6/latest/${currency1}`)
+    return response.data.rates[currency2]
+  }
+
+  async function handleCurrent1(e){
+    selected1 = e.target.value
+    if(selected1 == selected2){
       result1 = 1
       result2 = 1
+      currency1 = await getCurrency(selected1, selected2)
     } else {
+      flag = true
       result1 = 1
-      let exchange = round(currency1 > currency2 ? currency1: currency2 / currency1, 2)
-      result2 = round(result1 * exchange, 2)
+      currency1 = await getCurrency(selected1, selected2)
+      result2 = round(result1 * currency1, 3)
     }
   }
 
-  function handleCurrent2(e){
-    currency2 = e.target.value
-    console.log(currency1 == currency2)
-    if(currency1 == currency2){
+  async function handleCurrent2(e){
+    selected2 = e.target.value
+    if(selected1 == selected2){
       result1 = 1
       result2 = 1
+      currency2 = await getCurrency(selected2, selected1)
     } else {
+      flag = false
       result2 = 1
-      let exchange = round(currency1 > currency2 ? currency1 / currency2 : currency2 / currency1, 2)
-      result1 = round(result2 * exchange , 2)
+      currency2 = await getCurrency(selected2, selected1)
+      result1 = round(result2 * currency2 , 3)
     }
   }
 
   function res1(e){
     result1 = e.target.value
-    if(currency1 >= currency2){
-      result2 = round(result1 * currency1, 2)
-    } else {
-      result2 = round(result1 / (currency2 / currency1 ), 2)
-    }
+    if(selected1 == selected2)
+      result2 = result1
+    else {
+      if(flag)
+        result2 = round(result1 * currency1, 3)
+      else 
+        result2 = round(result1 / currency2, 3)
+    } 
   }
 
   function res2(e){
     result2 = e.target.value
-    if(currency1 >= currency2){
-      result1 = round(result2 / currency1, 2)
-    } else {
-      result1 = round(result2 * (currency2 / currency1 ), 2)
+    if(selected1 == selected2)
+      result1 = result2
+    else {
+      if(!flag)
+        result1 = round(result2 * currency2, 3)
+      else 
+        result1 = round(result2 / currency1, 3)
     }
-    
   }
-
-
-  // 1=97
-  // 1.07=90
-  
 
 </script>
 
@@ -62,33 +76,21 @@
   <div class="card">
     <div>
       <select on:change={handleCurrent1}>
-        <option value="90">usd</option>
-        <option value="1">rub</option>
-        <option value="97">euro</option>
+        <option value="RUB">RUB</option>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
       </select>
-
       <input type="number" value={result1} on:change={res1}>
     </div>
 
     <div>
       <select on:change={handleCurrent2}>
-        <option value="1">rub</option>
-        <option value="90">usd</option>
-        <option value="97">euro</option>
+        <option value="RUB">RUB</option>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
       </select>
-
       <input type="number" value={result2} on:change={res2}>
     </div>
-  </div>
-
-  <div>
-    {currency1}
-    {result1}
-  </div>
-
-  <div>
-    {currency2}
-    {result2}
   </div>
 </main>
 
