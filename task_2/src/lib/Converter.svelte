672b35add836f0a1d35edd498347c7c95e357aa6
split73 <54321lexa12345@mail.ru>
2024-05-13 13:28:00 +0400

    task_2 - done

--- task_2/src/lib/Converter.svelte
+++ task_2/src/lib/Converter.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
+	import CurrencyInput from "./components/CurrencyInput.svelte";
+
 	let firstCurrencyAmount = 0;
 	let secondCurrencyAmount = 0;
-	let firstSelectedCurrency: string;
-	let secondSelectedCurrency: string;
-	let currencyToExchange: string = "USD";
+	let firstSelectedCurrency: string = 'USD';
+	let secondSelectedCurrency: string = 'USD';
+	let firstFetchedRates: any;
+	let secondFetchedRates: any;
 
-	const getExchangeRates = async () => {
-		const res = await fetch(`https://v6.exchangerate-api.com/v6/274062c683b64fd6c7857c64/latest/${currencyToExchange}`);
+	async function getExchangeRates(currencyToExchange: string) {
+		const res = await fetch(
+			`https://v6.exchangerate-api.com/v6/274062c683b64fd6c7857c64/latest/${currencyToExchange}`
+		);
 		const data = await res.json();
+		firstCurrencyAmount = 0;
+		secondCurrencyAmount = 0;
 		return data;
+	}
+
+	const firstExchangeRates = (firstSelectedCurrency: string) => {
+		let tmpRates = getExchangeRates(firstSelectedCurrency).then(
+			(data) => (firstFetchedRates = data)
+		);
+		return tmpRates;
+	};
+	const secondExchangeRates = (secondSelectedCurrency: string) => {
+		let tmpRates = getExchangeRates(secondSelectedCurrency).then(
+			(data) => (secondFetchedRates = data)
+		);
+		return tmpRates;
 	};
 
 	function converte(
@@ -17,30 +37,59 @@
 		order: 'firstToSecond' | 'secondToFirst'
 	) {
 		if (order === 'firstToSecond') {
-			secondCurrencyAmount = 
+			firstCurrencyAmount =
+				secondFetchedRates.conversion_rates[firstSelectedCurrency] * secoondCurr;
+		} else {
+			secondCurrencyAmount = firstFetchedRates.conversion_rates[secondSelectedCurrency] * firstCurr;
 		}
 	}
 </script>
 
-{#await getExchangeRates()}
-	<p>awaiting...</p>
+{#await firstExchangeRates(firstSelectedCurrency)}
+	<select>
+		<option>awaiting...</option>
+	</select>
+	<input />
 {:then data}
-	<select bind:value={firstSelectedCurrency}>
+	<select
+		bind:value={firstSelectedCurrency}
+		on:change={() => getExchangeRates(firstSelectedCurrency)}
+	>
 		{#each Object.entries(data.conversion_rates) as [key, value]}
 			<option value={key}>
 				{key}
 			</option>
 		{/each}
 	</select>
-	<input bind:value={firstCurrencyAmount} on:input={() => console.log()} />
-	<select bind:value={secondCurrencyAmount}>
-		{#each Object.entries(data.conversion_rates) as [key, value]}
-			<option value={key}>
-				{key}
-			</option>
-		{/each}
-	</select>
-	<input bind:value={secondCurrencyAmount} on:input={() => converte(firstCurrencyAmount, secondCurrencyAmount, 'secondToFirst')} />
+	<input
+		bind:value={firstCurrencyAmount}
+		on:input={() => converte(firstCurrencyAmount, secondCurrencyAmount, 'secondToFirst')}
+	/>
 {:catch error}
 	<p>oh noes.</p>
 {/await}
+
+{#await secondExchangeRates(secondSelectedCurrency)}
+	<select>
+		<option>awaiting...</option>
+	</select>
+	<input />
+{:then data}
+	<select
+		bind:value={secondSelectedCurrency}
+		on:change={() => getExchangeRates(secondSelectedCurrency)}
+	>
+		{#each Object.entries(data.conversion_rates) as [key, value]}
+			<option value={key}>
+				{key}
+			</option>
+		{/each}
+	</select>
+	<input
+		bind:value={secondCurrencyAmount}
+		on:input={() => converte(firstCurrencyAmount, secondCurrencyAmount, 'firstToSecond')}
+	/>
+{:catch error}
+	<p>oh noes.</p>
+{/await}
+
